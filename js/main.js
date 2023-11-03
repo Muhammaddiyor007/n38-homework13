9e5babd3a9ccdaaad66f45fed9ab9e6ba3f48d0f
Muhammaddiyor007 <odiljonovmuhammaddiyor63@gmail.com>
2023-11-03 20:34:22 +0300

    n38-homework13

--- js/main.js
+++ js/main.js
@@ -8,3 +8,25 @@ modeBtn.addEventListener("click", function () {
     }
     document.body.classList.toggle("light");
 });
+const close_btn = document.querySelector(".close-btn");
+const open_btn = document.querySelector(".open-btn");
+const inner_nav_btns = document.querySelectorAll(".inner-nav-link");
+
+const nav = document.querySelector(".inner-nav");
+
+open_btn.addEventListener("click", () => {
+  nav.style.transform = "translateX(100%)";
+  open_btn.style.display = "none";
+  close_btn.style.display = "block";
+});
+close_btn.addEventListener("click", () => {
+  nav.style.transform = "translateX(-100%)";
+  close_btn.style.display = "none";
+  open_btn.style.display = "block";
+});
+
+inner_nav_btns.forEach((item) => {
+  item.addEventListener("click", () => {
+    nav.style.transform = "translateX(-100%)";
+  });
+});
