0b79819b7a12daaddcd64b3e874a4122bbd9fc87
Muhammaddiyor007 <odiljonovmuhammaddiyor63@gmail.com>
2023-11-04 01:00:12 +0300

    n38-homework13

--- js/main.js
+++ js/main.js
@@ -30,3 +30,14 @@ inner_nav_btns.forEach((item) => {
     nav.style.transform = "translateX(-100%)";
   });
 });
+
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "images/dark.svg";
+  } else {
+    this.firstElementChild.src = "images/light.svg";
+  }
+  document.body.classList.toggle("light");
+});
